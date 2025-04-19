f456bac368919bb6d7d29b0af52bef9eb55e9ff0
ISKO0 <51124377+ISKO0@users.noreply.github.com>
2025-04-19 08:06:37 +0300

    Add files via upload

--- arabic_currency_to_word/models.py
+++ arabic_currency_to_word/models.py
@@ -1,5 +1,17 @@
 class Currency:
-    def __init__(self, code, singular, dual, plural, fraction_singular, fraction_dual, fraction_plural, part_precision=2):
+    def __init__(
+        self,
+        code,
+        singular,
+        dual,
+        plural,
+        fraction_singular,
+        fraction_dual,
+        fraction_plural,
+        part_precision=2,
+        en_name="Unknown Currency",
+        en_fraction_name="subunit"
+    ):
         self.code = code
         self.singular = singular
         self.dual = dual
@@ -8,3 +20,5 @@ class Currency:
         self.fraction_dual = fraction_dual
         self.fraction_plural = fraction_plural
         self.part_precision = part_precision
+        self.en_name = en_name
+        self.en_fraction_name = en_fraction_name
